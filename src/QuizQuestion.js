7d577bc404922c1c2baa4afe434804eb4c3f33ba
allanc93 <45591897+allanc93@users.noreply.github.com>
2020-06-25 18:09:53 +0100

    Update QuizQuestion.js

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -37,7 +37,7 @@ class QuizQuestion extends Component {
                         })}
                     </ul>
                 </section>
-                {this.state.answer ? <p className="error">Sorry, that's not right</p> : null}
+                {this.state.incorrectAnswer ? <p className="error">Sorry, that's not right</p> : null}
             </main>
         )
     }
