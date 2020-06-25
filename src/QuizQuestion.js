a713deaa5818db519e26cd889de12d6b4c850b3d
allanc93 <45591897+allanc93@users.noreply.github.com>
2020-06-25 18:08:32 +0100

    Update to add incorrectAnswer state

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -5,13 +5,20 @@ class QuizQuestion extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            incorrectAnswer: 'false'
+            incorrectAnswer: false
         }
     }
 
     handleClick(buttonText) {
         if (buttonText === this.props.quiz_question.answer) {
+            this.setState({
+                incorrectAnswer: false
+            })
             this.props.showNextQuestionHandler()
+        } else {
+            this.setState({
+                incorrectAnswer: true
+            })
         }
     }
     render() {
@@ -30,6 +37,7 @@ class QuizQuestion extends Component {
                         })}
                     </ul>
                 </section>
+                {this.state.answer ? <p className="error">Sorry, that's not right</p> : null}
             </main>
         )
     }
--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -4,6 +4,7 @@ class QuizQuestionButton extends Component {
     handleClick() {
         this.props.clickHandler(this.props.button_text)
     }
+    
     render() {
         return (
             <li><button onClick={this.handleClick.bind(this)} >{this.props.button_text}</button></li>
